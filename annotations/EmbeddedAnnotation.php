acfe9c8ac1b59c54bf7c929366a8e650692f78e2
Peter Maselkowski <pmaselkowski@gmail.com>
2014-03-24 20:38:44 +0100

    + Added possible params for embedded annotation

--- annotations/EmbeddedAnnotation.php
+++ annotations/EmbeddedAnnotation.php
@@ -4,6 +4,12 @@
  * Annotation for embedded document in mongo
  * defaultClassName will be used for getting empty properties,
  * but any type of embedded document can be stored within this field
+ * Examples:
+ * <ul>
+ *		<li><b>Embedded('EmbeddedClassName')</b>: Embed with whatever set</li>
+ *		<li><b>Embedded('EmbeddedClassName')</b>: Embed with default class</li>
+ *		<li><b>Embedded({'EmbeddedClassName', params...})</b>: Embed with default class and optional params (currently none)</li>
+ * </ul>
  * @Target('property')
  * @template Embedded('${defaultClassName}')
  * @author Piotr
@@ -14,7 +20,18 @@ class EmbeddedAnnotation extends EComponentMetaAnnotation
 	
 	public function init()
 	{
-		$this->_entity->embedded = $this->value;
+		$params = new stdClass();
+		if(is_array($this->value))
+		{
+			$className = array_shift($this->value);
+			$params = (object)$this->value;
+		}
+		else
+		{
+			$className = $this->value;
+		}
+		$this->_entity->embedded = $className;
+		$this->_entity->embeddedParams = $this->value;
 		$this->_entity->direct = false;
 	}
 }
