ba29f4b3e9ffad6b6c9bd6ad620521f36eac5188
Peter Maselkowski <pmaselkowski@gmail.com>
2014-12-19 13:13:17 +0100

    * phpdoc fix

--- Maslosoft/Addendum/Interfaces/IMetaAnnotation.php
+++ Maslosoft/Addendum/Interfaces/IMetaAnnotation.php
@@ -12,18 +12,21 @@ interface IMetaAnnotation
 {
 	/**
 	 * Set entity name (class name, method name or property name)
+	 * @return void
 	 */
 	public function setName($name);
 
 	/**
 	 * Set metada class to be accessible for annotation for init etc. methods
 	 * @param Meta $meta
+	 * @return void
 	 */
 	public function setMeta(Meta $meta);
 
 	/**
 	 * Set annotatins entity, it can be either class, property, or method
 	 * @param IAnnotationEntity $entity
+	 * @return void
 	 */
 	public function setEntity(IAnnotationEntity $entity);
 
@@ -31,6 +34,7 @@ interface IMetaAnnotation
 	 * This function should be called after all annotations are initialized.
 	 * Any code that depends on other annotations can be executed here.
 	 * NOTE: This is not ensured to run, its annotations container responsibility to call it.
+	 * @return void
 	 */
 	public function afterInit();
 }
