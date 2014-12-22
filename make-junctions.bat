eda5eb57e9480a4d034dd41de96e79c484123fa3
Peter Maselkowski <pmaselkowski@gmail.com>
2014-12-22 21:32:44 +0100

    -! Moved annotations to proper projects

--- make-junctions.bat
+++ make-junctions.bat
@@ -1,3 +1,3 @@
 mklink /j unit vendor\maslosoft\addendumtest\tests\unit
 mklink /j models vendor\maslosoft\addendumtest\Maslosoft\AddendumTest\Models
-mklink /j annt vendor\maslosoft\addendumtest\Maslosoft\AddendumTest\Annotations
+mklink /j annotations vendor\maslosoft\addendumtest\Maslosoft\AddendumTest\Annotations
