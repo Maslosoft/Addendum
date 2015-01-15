915dd566e56070d190e5cfe9fee0908855ef9686
Peter Maselkowski <pmaselkowski@gmail.com>
2015-01-15 13:40:39 +0100

    - Removed gii generators

--- gii/netBeans/views/index.php
+++
@@ -1,6 +0,0 @@
-<h1>NetBeans annotations autocomplete file</h1>
-<p>Import generated file into NetBeans settings to get autocompletition for annotations</p>
-
-<? $form = $this->beginWidget('CCodeForm', array('model' => $model)); ?>
-
-<? $this->endWidget(); ?>
--- gii/validator/views/index.php
+++
@@ -1,6 +0,0 @@
-<h1>Generate annotations for validators</h1>
-<p>This will generate annotations for existing validator classes.
-	Specialized annotations for validators are not required, however it is
-	somewhat more convenient to write ie. <code>@RequiredValidator</code> than
-	<code>@Validator('required')</code>, or not?
-</p>
