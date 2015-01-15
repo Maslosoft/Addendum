32c01cf2f08280d8b91a96ee31ec6a8ce879db5d
Peter Maselkowski <pmaselkowski@gmail.com>
2015-01-15 10:44:47 +0100

    - Removed yii validator converter, see https://github.com/Maslosoft/AddendumValidatorGenerator

--- src/Utilities/AnnotationUtility.php
+++ src/Utilities/AnnotationUtility.php
@@ -186,53 +186,6 @@ class AnnotationUtility
 	}
 
 	/**
-	 * Generate validator annotations from existing validator classes
-	 */
-	public function convertValidators()
-	{
-		$v = CValidator::$builtInValidators;
-		$template = <<<CODE
-<?php
-/**
- * NOTE: This class is automatically generated from Yii validator class.
- * This is not actual validator. For validator class @see C%2\$s.
- */
-%1\$s
-class %2\$sAnnotation extends EValidatorAnnotation implements IBuiltInValidatorAnnotation
-{
-%3\$s
-}
-CODE;
-		$ignored = [];
-		$info = new ReflectionClass(ValidatorAnnotation::class);
-		foreach ($info->getProperties(ReflectionProperty::IS_PUBLIC) as $field)
-		{
-			$ignored[$field->name] = $field->name;
-		}
-		$ignored['attributes'] = true;
-		$ignored['builtInValidators'] = true;
-		foreach ($v as $n => $class)
-		{
-			$name = ucfirst($n) . 'Validator';
-			$info = new ReflectionAnnotatedClass($class);
-			$classComment = $info->getDocComment();
-			$values = $info->getDefaultProperties();
-			$fields = [];
-			foreach ($info->getProperties(ReflectionProperty::IS_PUBLIC) as $field)
-			{
-				if (isset($ignored[$field->name]))
-				{
-					continue;
-				}
-				$comment = $field->getDocComment();
-				$fields[$field->name] = sprintf("\t%s\n\tpublic \$%s = %s;\n", $comment, $field->name, var_export($values[$field->name], true));
-			}
-			$code = sprintf($template, $classComment, $name, implode("\n", $fields));
-			file_put_contents("c:/temp/{$name}Annotation.php", $code);
-		}
-	}
-
-	/**
 	 * This utility method find files containing $annotations
 	 * annotates them and performs callback
 	 * @param string[] $annotations
