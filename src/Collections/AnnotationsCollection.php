3a3e04cb598aef8a233d1f3cbbfc3264605951b8
Peter Maselkowski <pmaselkowski@gmail.com>
2015-03-09 11:29:01 +0100

    * Class headers

--- src/Collections/AnnotationsCollection.php
+++ src/Collections/AnnotationsCollection.php
@@ -1,4 +1,17 @@
 <?php
+
+/**
+ * This software package is licensed under New BSD license.
+ *
+ * @package maslosoft/addendum
+ * @licence New BSD
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com> (Meta container, further improvements, bugfixes)
+ * @copyright Copyright (c) Maslosoft (Meta container, further improvements, bugfixes)
+ * @copyright Copyright (c) Jan Suchal (Original version, builder, parser)
+ * @link http://maslosoft.com/addendum/ - maslosoft addendum
+ * @link https://code.google.com/p/addendum/ - original addendum project
+ */
+
 namespace Maslosoft\Addendum\Collections;
 
 use Maslosoft\Addendum\Addendum;
---
+++ toc/file-header.phps
@@ -0,0 +1,9 @@
+This software package is licensed under {{composer.license}} license.
+
+@package {{composer.name}}
+@licence {{composer.license}}
+@copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com> (Meta container, further improvements, bugfixes)
+@copyright Copyright (c) Maslosoft (Meta container, further improvements, bugfixes)
+@copyright Copyright (c) Jan Suchal (Original version, builder, parser)
+@link http://maslosoft.com/addendum/ - maslosoft addendum
+@link https://code.google.com/p/addendum/ - original addendum project
