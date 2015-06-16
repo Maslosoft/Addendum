006c32dfea03a3acd66a2a7ed9c7c149004cea06
Peter Maselkowski <pmaselkowski@gmail.com>
2015-06-16 10:50:28 +0200

    * make links sh

---
+++ make-junctions.sh
@@ -0,0 +1,4 @@
+ln -s vendor/maslosoft/addendumtest/tests/unit unit
+ln -s vendor/maslosoft/addendumtest/models models
+ln -s vendor/maslosoft/addendumtest/annotations annotations
+ln -s vendor/maslosoft/addendumtest/misc misc
