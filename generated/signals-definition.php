baeee52c799a4366497e0e6ac17502fcb2612087
Peter Maselkowski <pmaselkowski@gmail.com>
2015-12-16 16:24:56 +0100

    >> External travis script, misc test cli changes

---
+++ generated/signals-definition.php
@@ -0,0 +1,16 @@
+<?php // Auto generated, any changes will be lost
+return array (
+  'slots' => 
+  array (
+  ),
+  'signals' => 
+  array (
+    '\\Maslosoft\\Addendum\\Signals\\NamespacesSignal' => 
+    array (
+      '\\Maslosoft\\AddendumTest\\Annotations\\SignaledNs\\SlotForNamespace' => 
+      array (
+        0 => 'reactOn()',
+      ),
+    ),
+  ),
+);
