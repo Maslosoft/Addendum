726ef64013d6d4fc4c943650e256e3aac490a8c5
Peter Maselkowski <pmaselkowski@gmail.com>
2016-07-20 09:42:42 +0200

    +~ Docs

---
+++ docs/build-in/index.md.php
@@ -0,0 +1,20 @@
+<?php
+
+use Maslosoft\Addendum\Annotations\TargetAnnotation;
+use Maslosoft\Zamm\Iterator;
+use Maslosoft\Zamm\DocBlock;
+?>
+<title>Built-in annotations</title>
+
+# Built-in annotations
+
+Addendum contains several built-in annotation, which are meant to be used on
+other annotations. These define constraints of annotations usage.
+
+<?php
+
+foreach (Iterator::ns(TargetAnnotation::class) as $class):
+	echo '###' . str_replace('Annotation', '', (new ReflectionClass($class))->getShortName()) . PHP_EOL;
+	echo new DocBlock($class);
+endforeach;
+?>
---
+++ docs/index.md.php
@@ -0,0 +1,14 @@
+<title>Documentation</title>
+
+# Documentation
+
+There are several aspects of addendum project which can be divided into basic and advanced usage.
+
+### Basic principles
+
+1. Annotating objects
+2. Creating annotations
+
+### More advanced features
+
+1. Using custom meta container
