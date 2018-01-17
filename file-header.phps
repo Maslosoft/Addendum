41f7c95a334ff87657aa03ce12498c98b21c269d
Peter Maselkowski <office@maslosoft.com>
2018-01-17 18:58:06 +0100

    * Class headers

--- file-header.phps
+++ file-header.phps
@@ -5,5 +5,5 @@ This software package is licensed under AGPL, Commercial license.
 @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com> (Meta container, further improvements, bugfixes)
 @copyright Copyright (c) Maslosoft (Meta container, further improvements, bugfixes)
 @copyright Copyright (c) Jan Suchal (Original version, builder, parser)
-@link http://maslosoft.com/addendum/ - maslosoft addendum
+@link https://maslosoft.com/addendum/ - maslosoft addendum
 @link https://code.google.com/p/addendum/ - original addendum project
--- src/Exceptions/ClassNotFoundException.php
+++ src/Exceptions/ClassNotFoundException.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * This software package is licensed under AGPL, Commercial license.
+ *
+ * @package maslosoft/addendum
+ * @licence AGPL, Commercial
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com> (Meta container, further improvements, bugfixes)
+ * @copyright Copyright (c) Maslosoft (Meta container, further improvements, bugfixes)
+ * @copyright Copyright (c) Jan Suchal (Original version, builder, parser)
+ * @link https://maslosoft.com/addendum/ - maslosoft addendum
+ * @link https://code.google.com/p/addendum/ - original addendum project
+ */
+
 namespace Maslosoft\Addendum\Exceptions;
 
 use Exception;
