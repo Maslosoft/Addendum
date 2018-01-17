a4be649311ce8daaf9f5540d292fb3e7c3eecf92
Peter Maselkowski <office@maslosoft.com>
2018-01-17 18:58:06 +0100

    * Class headers

--- src/Matcher/StringMatcher.php
+++ src/Matcher/StringMatcher.php
@@ -8,7 +8,7 @@
  * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com> (Meta container, further improvements, bugfixes)
  * @copyright Copyright (c) Maslosoft (Meta container, further improvements, bugfixes)
  * @copyright Copyright (c) Jan Suchal (Original version, builder, parser)
- * @link http://maslosoft.com/addendum/ - maslosoft addendum
+ * @link https://maslosoft.com/addendum/ - maslosoft addendum
  * @link https://code.google.com/p/addendum/ - original addendum project
  */
 
