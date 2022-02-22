45644dbd097654937489e0731fa2e0c27003d8e4
Peter Maselkowski <office@maslosoft.com>
2022-02-22 18:39:40 +0100

    + str_contains polyfill

--- src/polyfills.php
+++ src/polyfills.php
@@ -1,6 +1,14 @@
 <?php
 
-if(!defined('T_NAME_QUALIFIED'))
+if (!defined('T_NAME_QUALIFIED'))
 {
 	define('T_NAME_QUALIFIED', 265);
+}
+// based on original work from the PHP Laravel framework
+if (!function_exists('str_contains'))
+{
+	function str_contains($haystack, $needle): bool
+	{
+		return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+	}
 }
