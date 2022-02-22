d979f44bfc53a4c99e3c703aa7084df09c2c4512
Peter Maselkowski <office@maslosoft.com>
2022-02-22 16:17:05 +0100

    >> Modernize * Fixed version displaying

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -5,11 +5,10 @@ use Maslosoft\AddendumTest\Annotations\NamespacedAnnotation;
 use Maslosoft\Signals\Signal;
 use Maslosoft\Signals\Utility;
 
-error_reporting(E_ALL);
+error_reporting(E_ALL ^ E_DEPRECATED);
 date_default_timezone_set('Europe/Paris');
 
-define('VENDOR_DIR', __DIR__ . '/../vendor');
-//define('YII_DIR', VENDOR_DIR . '/yiisoft/yii/framework/');
+const VENDOR_DIR = __DIR__ . '/../vendor';
 require VENDOR_DIR . '/autoload.php';
 
 // Invoker stub for windows
@@ -18,22 +17,17 @@ if (defined('PHP_WINDOWS_VERSION_MAJOR'))
 	require __DIR__ . '/misc/Invoker.php';
 }
 
-//require $yii = YII_DIR . 'yii.php';
-
-//require_once YII_DIR . 'base/CComponent.php';
-//require_once YII_DIR . 'base/CModel.php';
-
 $config = require __DIR__ . '/config.php';
 
 require_once __DIR__ . '/misc/AddendumWipe.php';
 require_once __DIR__ . '/misc/NonNamespaced.php';
 require_once __DIR__ . '/misc/NonNamespacedAnnotation.php';
 
-define('TEST_CONSTANT', 'Global constant');
+const TEST_CONSTANT = 'Global constant';
 
-define('ANNOTATIONS_PATH', __DIR__ . '/annotations');
-define('MODELS_PATH', __DIR__ . '/models');
-define('RUNTIME_PATH', __DIR__ . '/../runtime');
+const ANNOTATIONS_PATH = __DIR__ . '/annotations';
+const MODELS_PATH = __DIR__ . '/models';
+const RUNTIME_PATH = __DIR__ . '/../runtime';
 
 $signal = new Signal();
 $signal->runtimePath = RUNTIME_PATH;
@@ -46,4 +40,4 @@ $addendum->namespaces[] = NamespacedAnnotation::Ns;
 $addendum->init();
 
 // Here you can initialize variables that will be available to your tests
-echo "Addendum " . $addendum->getVersion();
+echo "Addendum " . $addendum->getVersion() . PHP_EOL;
