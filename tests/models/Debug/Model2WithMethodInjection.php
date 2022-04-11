8a6d74e5c589561b4d9e4bed04e24abb3284e161
Peter Maselkowski <office@maslosoft.com>
2022-04-11 17:25:15 +0200

    >> Modernize

--- tests/models/Debug/Model2WithMethodInjection.php
+++ tests/models/Debug/Model2WithMethodInjection.php
@@ -9,7 +9,7 @@
 namespace Maslosoft\AddendumTest\Models\Debug;
 
 use Maslosoft\Addendum\Interfaces\AnnotatedInterface;
-use Maslosoft\Signals\ISignal;
+use Maslosoft\Signals\Interfaces\SignalInterface;
 
 /**
  * ModelWithMethodInjection
@@ -23,9 +23,9 @@ class Model2WithMethodInjection implements AnnotatedInterface
 	 * @SlotFor(Maslosoft\SignalsTest\Signals\MethodInjected)
 	 * @SlotFor(Maslosoft\SignalsTest\Signals\MethodInjected2)
 	 * @SignalFor(Blah)
-	 * @param ISignal $signal
+	 * @param SignalInterface $signal
 	 */
-	public function on(ISignal $signal)
+	public function on(SignalInterface $signal)
 	{
 		$signal->emited = true;
 	}
